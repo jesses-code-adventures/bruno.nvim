7c48a2729defda5ad2c37c838fbe7b27019fb2a3
Jesse Williams <jesse@negativespacesounds.com>
2025-04-15 01:07:25 +1000

    curl requests working!

---
+++ lua/bruno/parser.lua
@@ -0,0 +1,255 @@
+local M = {}
+
+---@class bruno.parser.dictionary_block
+---@field key string
+---@field values bruno.parser.dictionary_entry[]
+
+---@class bruno.parser.array_block
+---@field key string
+---@field values bruno.parser.array_entry[]
+
+---@class bruno.parser.text_block
+---@field key string
+---@field value string
+
+
+---@class bruno.parser.dictionary_entry
+---@field key string
+---@field value string
+---@field disabled boolean
+
+---@class bruno.parser.array_entry
+---@field value string
+---@field disabled boolean
+
+---@class bruno.parser.request
+---@field method string
+---@field url string
+---@field body string?
+---@field auth string?
+
+--- actual bruno types
+
+---@class bruno.parser.meta
+---@field name string
+---@field type string?
+---@field seq number -- sort order
+
+---@class bruno.parser.bru_file_contents
+---@field meta bruno.parser.meta?
+---@field get bruno.parser.request?
+---@field post bruno.parser.request?
+---@field put bruno.parser.request?
+---@field delete bruno.parser.request?
+---@field options bruno.parser.request?
+---@field trace bruno.parser.request?
+---@field connect bruno.parser.request?
+---@field head bruno.parser.request?
+---@field query bruno.parser.dictionary_entry[]? params:query
+---@field path bruno.parser.dictionary_entry[]? params:path
+---@field headers bruno.parser.dictionary_entry[]?
+---@field body table?
+---@field body_text string? treat as text
+---@field body_xml table?
+---@field body_form-urlencoded bruno.parser.dictionary_entry[]?
+---@field body_multipart-form-data bruno.parser.dictionary_entry[]?
+---@field body_graphql table?
+---@field body_graphql_vars table?
+---@field script_pre-request string? -- javascript
+---@field script_post-post-response string? -- javascript
+---@field test string? -- test
+
+local test_data = [[
+    meta {
+      name: Create a magic link
+      type: http
+      seq: 4
+    }
+
+    post {
+      url: {{baseUrl}}/api/v3/{{id}}/link
+      body: none
+      auth: none
+    }
+
+    params:query {
+      ~destination_url:
+    }
+
+    params:path {
+      firm_id:
+    }
+
+
+    tests {
+      test("status must be 200", function() {
+          expect(res.status).to.eql(200);
+      });
+
+      test("expiry time is valid", function() {
+        // Check that property exists and is not empty
+      });
+
+      test("url is properly formatted", function() {
+        expect(res.body).to.have.property('url').that.is.not.empty;
+      });
+    }
+]]
+
+---@param content string[]
+---@return bruno.parser.bru_file_contents
+function M.parse_bru_content(content)
+    ---@type bruno.parser.bru_file_contents
+    local result = {}
+    local current_block = nil
+
+    for _, line in ipairs(content) do
+        line = line:match("^%s*(.-)%s*$") -- trim
+
+        if line == "" then goto continue end
+
+        if line == "}" then
+            current_block = nil
+            goto continue
+        end
+
+        local block = line:match("^(%w+)%:?%w*%s*%{$")
+        if block and block ~= "tests" then
+            current_block = block
+            result[current_block] = result[current_block] or {}
+            goto continue
+        elseif block == "tests" then
+            current_block = "skip"
+            goto continue
+        end
+
+        -- key: value
+        if current_block and current_block ~= "skip" then
+            local k, v = line:match("^(.-):%s*(.-)%s*$")
+            if k and v then
+                result[current_block][k] = v
+            end
+        end
+
+        ::continue::
+    end
+
+    return result
+end
+
+function M.get_dbg()
+    return M.dbg
+end
+
+function M._print(msg)
+    M.utils.Print(M, msg)
+end
+
+---@param bru_file bruno.parser.bru_file_contents
+---@return string[]
+function M.generate_curl_command(bru_file)
+    local args = { "curl" }
+
+    -- method
+    local method = bru_file.get and "get" or bru_file.post and "post" or bru_file.put and "put" or
+    bru_file.delete and "delete" or bru_file.options and "options" or bru_file.head and "head" or "get"
+    table.insert(args, "-X")
+    table.insert(args, method:upper())
+
+    -- headers
+    if bru_file.headers then
+        for _, header in ipairs(bru_file.headers) do
+            if not header.disabled then
+                table.insert(args, "-H")
+                table.insert(args, header.key .. ": " .. header.value)
+            end
+        end
+    end
+
+    -- url and query params
+    local url = bru_file[method] and bru_file[method].url or ""
+    url = M.format_url(url)
+    if bru_file.query then
+        local query_params = {}
+        for _, param in ipairs(bru_file.query) do
+            if not param.disabled then
+                table.insert(query_params, param.key .. "=" .. param.value)
+            end
+        end
+        if #query_params > 0 then
+            url = url .. "?" .. table.concat(query_params, "&")
+        end
+    end
+    table.insert(args, url)
+
+    -- body
+    if bru_file.post and bru_file.post.body or bru_file.put and bru_file.put.body then
+        local content_type = ""
+        local data = ""
+
+        if bru_file["body_form-urlencoded"] then
+            content_type = "application/x-www-form-urlencoded"
+            local form_data = {}
+            for _, item in ipairs(bru_file["body_form-urlencoded"]) do
+                if not item.disabled then
+                    table.insert(form_data, item.key .. "=" .. item.value)
+                end
+            end
+            data = table.concat(form_data, "&")
+        elseif bru_file.body_text then
+            content_type = "text/plain"
+            data = bru_file.body_text
+        elseif bru_file.body_xml then
+            content_type = "application/xml"
+            data = bru_file.body_xml -- TODO: serialize xml
+        elseif bru_file["body_multipart-form-data"] then
+            -- TODO: implement
+        elseif bru_file.body_graphql then
+            -- TODO: implement
+        elseif bru_file.body then
+            content_type = "application/json"
+            data = vim.json.encode(bru_file.body)
+        end
+
+        if content_type ~= "" then
+            table.insert(args, "-H")
+            table.insert(args, "content-type: " .. content_type)
+        end
+
+        if data ~= "" then
+            table.insert(args, "--data")
+            table.insert(args, data)
+        end
+    end
+
+    print("generated curl command args --->", vim.inspect(args))
+    return args
+end
+
+---@param env_file string
+function M.parse_bru_environment_vars(env_file)
+    M._selected_env_variables = {}
+    local env = M.parse_bru_content(vim.fn.readfile(env_file))
+    for k, v in pairs(env) do
+        if k == "vars" then
+            for key, value in pairs(v) do
+                M._selected_env_variables[key] = value
+            end
+        end
+    end
+    return M._selected_env_variables
+end
+
+---@param opts bruno.setup.opts
+---@param format_url function(string): string
+---@return self
+function M.setup(opts, format_url)
+    M.mod_name = "PARSER"
+    M.utils = require("bruno.utils")
+    M = M.utils.Setup_module(M, opts)
+
+    M.format_url = format_url
+    return M
+end
+
+return M
--- lua/bruno/queries.lua
+++ lua/bruno/queries.lua
@@ -37,6 +37,13 @@ function M._is_bru_file(filepath)
     return true
 end
 
+---@param dir string
+function M._set_collection_dir(dir)
+    M._collection_dir = dir
+    -- TODO: remove this
+    M._sync_environment()
+end
+
 ---@param filepath string
 function M.set_collection_from_query(filepath)
     local parent = vim.fn.fnamemodify(filepath, ":h")
@@ -44,7 +51,7 @@ function M.set_collection_from_query(filepath)
         M._print("trying to find collection.bru in " .. parent)
         local stat = vim.loop.fs_stat(vim.fs.joinpath(parent, "collection.bru"))
         if stat and stat.type == "file" then
-            M._collection_dir = parent
+            M._set_collection_dir(parent)
             return
         end
         parent = vim.fn.fnamemodify(parent, ":h")
@@ -52,8 +59,31 @@ function M.set_collection_from_query(filepath)
     error("collection.bru not found")
 end
 
---- query the current file, if it's a .bru file
 function M.query_file()
+    local current_file = vim.fn.expand("%:p")
+    if not M._is_bru_file(current_file) then
+        M._print("not a .bru file")
+        return
+    end
+
+    M.set_collection_from_query(current_file)
+
+    if not M._query_win or not vim.api.nvim_win_is_valid(M._query_win) then
+        M._create_response_window()
+    end
+
+    local file_contents = M.parser.parse_bru_content(vim.fn.readfile(current_file))
+    local args = M.parser.generate_curl_command(file_contents)
+    M._print("making query from cwd " ..
+        M._collection_dir .. " with file " .. current_file .. " and args " .. vim.inspect(args))
+    vim.schedule(function()
+        local resp = M.utils.Syscall(args, { cwd = M._collection_dir }, function() end)
+        M._render_response(resp)
+    end)
+end
+
+--- query the current file, if it's a .bru file
+function M.bru_run_file()
     local current_file = vim.fn.expand("%:p")
     if not M._is_bru_file(current_file) then
         M._print("not a .bru file")
@@ -68,7 +98,7 @@ function M.query_file()
     vim.schedule(function()
         local args = M._cmd_bru_run(current_file)
         M._print("making query from cwd " ..
-        M._collection_dir .. " with file " .. current_file .. " and args " .. vim.inspect(args))
+            M._collection_dir .. " with file " .. current_file .. " and args " .. vim.inspect(args))
         local resp = M.utils.Syscall(args, { cwd = M._collection_dir }, function() end)
         M._render_response(resp)
     end)
@@ -127,15 +157,77 @@ function M._render_response(resp)
     local output_string = ""
     if resp.code ~= 0 then
         output_string = output_string ..
-        "[FAILED]: Resp code - [" .. resp.code .. "]\n" .. (resp.stderr or "unknown error") .. "\n"
+            "[FAILED]: Resp code - [" .. resp.code .. "]\n" .. (resp.stderr or "unknown error") .. "\n"
     end
     output_string = output_string .. resp.stdout
     M._write_to_response_window({})
     M._write_to_response_window(vim.split(output_string, "\n"))
 end
 
+---@param url string
+---@return string
+function M.format_url(url)
+    local resp = url:gsub("{{(.-)}}", function(key)
+        return M._selected_env_variables[key] or error("missing var: " .. key)
+    end)
+    return resp
+end
+
 function M.get_dbg()
     return M.dbg
+end
+
+---@return string[]
+function M.get_environments()
+    if not M._collection_dir then
+        M._print("collection_dir not set")
+        return {}
+    end
+    local envs = M.utils.Syscall({ "ls", "environments" }, { cwd = M._collection_dir }, function() end)
+    if envs.code ~= 0 then
+        M._print("error getting environments")
+        return {}
+    end
+    local env_list = {}
+    for env in string.gmatch(envs.stdout, "[^\r\n]+") do
+        table.insert(env_list, env)
+    end
+    return env_list
+end
+
+function M._set_environment(env)
+    M._selected_env_name = env
+    M._sync_environment()
+end
+
+function M._sync_environment()
+    if not M._collection_dir then
+        M._print("collection_dir not set")
+        return
+    end
+    M._print("syncing environment " .. M._selected_env_name)
+    local env = M._selected_env_name
+    local env_file = vim.fs.joinpath(M._collection_dir, "environments", env .. ".bru")
+    if not vim.loop.fs_stat(env_file) then
+        M._print("environment file " .. env_file .. " does not exist")
+        return
+    end
+    M._selected_env_variables = M.parser.parse_bru_environment_vars(env_file)
+end
+
+function M._debug_queries()
+    M._print("selected env: " .. M._selected_env_name)
+    M._print("selected env variables: " .. vim.inspect(M._selected_env_variables))
+    M._print("collection dir: " .. (M._collection_dir or ""))
+    M._print("query args: " .. vim.inspect(M._query_args))
+    M._print("query win: " .. vim.inspect(M._query_win))
+    M._print("response win: " .. vim.inspect(M._response_win))
+    M._print("response buf: " .. vim.inspect(M._response_buf))
+    if not M._response_buf then
+        M._print("response buf not set")
+        return
+    end
+    M._print("response buf lines: " .. vim.inspect(vim.api.nvim_buf_get_lines(M._response_buf, 0, -1, false)))
 end
 
 ---@param opts bruno.setup.opts
@@ -144,7 +236,9 @@ function M.setup(opts)
     M.mod_name = "QUERIES"
     M.utils = require("bruno.utils")
     M = M.utils.Setup_module(M, opts)
-    M._selected_env_name = opts._selected_env_name or "localdev"
+
+    M.parser = require("bruno.parser").setup(opts, M.format_url)
+    M._set_environment(opts._selected_env_name or "localdev")
 
     M._query_args = {}
     M._print("initializing queries")
